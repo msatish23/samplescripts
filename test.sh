6b3850d3de5ca8668dfcf55cc227d13c38009820
msatish23 <msatish23@gmail.com>
2020-02-14 12:38:41 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 sudo docker images
 az login --identity -u /subscriptions/9540b342-9f94-4dd9-9eca-0698dda0107c/resourceGroups/cloudengg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/cloudengg
 az acr login --name cloudengg
-docker run -p 8081:80 -d cloudengg.azurecr.io/nginxdemos:v2
+docker run -p 8081:80 -d cloudengg.azurecr.io/nginxdemos:v1
